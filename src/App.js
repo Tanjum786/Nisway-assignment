2710625ae0c13fd69258c1e4ca9215a56a4449b3
tanjum786 <tanjunkadakol147@gmail.com>
2022-08-09 15:14:49 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setdata] = useState([]);
+
+  useEffect(
+    () => async () => {
+      try {
+        let response = await axios.get(
+          "https://techcrunch.com/wp-json/wp/v2/posts?per_page=10"
+        );
+        setdata(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    []
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((datas) => {
+        return (
+          <>
+            <span>date: {datas.date}</span>
+            <h1>title: {datas.title.rendered}</h1>
+            <h1>excerpt: {datas.excerpt.rendered}</h1>
+            <h1>author: {datas.parsely.meta.author.map((el) => el.name)}</h1>;
+          </>
+        );
+      })}
     </div>
   );
 }
