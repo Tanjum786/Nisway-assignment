5711abdebbd7b3d837692fd8cc62331a7c03328a
tanjum786 <tanjunkadakol147@gmail.com>
2022-08-10 17:34:32 +0530

    feat : navbar component created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FiGithub } from "react-icons/fi";
+import "./navbar.css";
+
+export const NavBar = () => {
+  return (
+    <>
+      <nav className="navContainer">
+        <NavLink to="/" className="navLink">
+          <h1 className="navTitle">News Articles</h1>
+        </NavLink>
+        <a
+          href="https://github.com/Tanjum786?tab=repositories"
+          target="_blank"
+          className="githubLink"
+          rel="noreferrer"
+          title="Github"
+        >
+          <FiGithub />
+        </a>
+      </nav>
+    </>
+  );
+};
