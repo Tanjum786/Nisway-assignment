f743d7822cb8f904ff7df36879803238a0bd1edc
tanjum786 <tanjunkadakol147@gmail.com>
2022-08-10 17:33:23 +0530

    feat : created custom folder for api calls

---
+++ src/Apicall/getdata.jsx
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export const getdata = async (setdata,setloading) => {
+    try {
+        setloading(true)
+        const response = await axios.get("https://techcrunch.com/wp-json/wp/v2/posts?per_page=10")
+        setdata(response.data)
+        setloading(false)
+    } catch (error) {
+
+    }
+}
